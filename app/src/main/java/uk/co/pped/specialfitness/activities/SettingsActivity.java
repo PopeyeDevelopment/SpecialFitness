4514547b7e89880c19bfc636000a636081995e2d
Matthew Inman <Matthew.Inman@cdl.co.uk>
2017-09-26 14:31:22 +0100

    Added NavigationBar/ActionBar to top of SettingsActivity

--- app/src/main/java/uk/co/pped/specialfitness/activities/SettingsActivity.java
+++ app/src/main/java/uk/co/pped/specialfitness/activities/SettingsActivity.java
@@ -1,11 +1,27 @@
 package uk.co.pped.specialfitness.activities;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import uk.co.pped.specialfitness.R;
 
 /**
  * Created by matthewi on 08/09/2017.
  */
 
 public class SettingsActivity extends AppCompatActivity {
+
+    public SettingsActivity() {}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setFocusableInTouchMode(true);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
 }
